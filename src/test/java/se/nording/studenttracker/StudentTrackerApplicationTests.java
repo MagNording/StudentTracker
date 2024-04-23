48b63ec6b01346b4e58d2e1626f0d7f9b94d36fd
Magnus Nording <magnus.nording@iths.se>
2024-04-23 19:29:56 +0200

    Initial commit

---
+++ src/test/java/se/nording/studenttracker/StudentTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package se.nording.studenttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
